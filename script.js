6987d6ca222dc2f8ec2906602f2e67b629f9195b
sados45 <sados45@gmail.com>
2022-06-24 00:42:57 +0200

    react kurulum,

---
+++ script.js
@@ -0,0 +1,10 @@
+//react i kurmak icin ilk önce node.js i kurduk. sonra vs code icinde termianli actik. terminain icinde npx -v ve npm -v taramasi yaptik. bundan sonra her projeden önce mutlaka reacti kulacagimiz calismalarin icin terminal icine npx create-react-app@5.0.1 my-app kurduk. calismamizin ismi my-app bu nedenle bu sekilde yazdik. 
+
+// daha cd my-app yaptik ve ardindan npm start yazdik. 
+//sayfalari kapatmis olsak dahi chromeda acilan localhost:3000 iyazdigimzda kurdugumuz proje tekrar kaldigi yerden devam ediyor. 
+
+//localhost projeleri 3000 lerde calisir. backend projeleri ise 8000 lerde calisir. 
+
+//bizim yazacagimiz kodlar src dosyasinin icerisinde yer alacak. 
+//node_modules önemli.
+//genelde images public dosyasinin icerisinde olur. genel itibarilye calisma alani src icindedir. 
